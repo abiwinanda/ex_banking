1e8683660a234c34b91b01e4b7debca5e96f3261
abiwinanda <abiwinanda@outlook.com>
2022-07-18 19:24:33 +0800

    create user genserver and supervisor

--- lib/ex_banking/users/user.ex
+++ lib/ex_banking/users/user.ex
@@ -1,2 +1,45 @@
 defmodule ExBanking.Users.User do
+  @moduledoc false
+  use GenServer
+
+  defmodule State do
+    defstruct pending_ops: 0
+  end
+
+  ##########
+  # Client #
+  ##########
+
+  def start_link(user),
+    do: GenServer.start_link(__MODULE__, %State{}, name: via(user))
+
+  def enqueue_operation(user),
+    do: GenServer.call(via(user), :enqueue_operation)
+
+  def dequeue_operation(user),
+    do: GenServer.call(via(user), :dequeue_operation)
+
+  ##########
+  # Server #
+  ##########
+
+  def init(state),
+    do: {:ok, state}
+
+  def handle_call(:enqueue_operation, _from, %State{pending_ops: pending_ops} = state)
+      when pending_ops < 10,
+      do: {:reply, {:ok, pending_ops + 1}, %State{state | pending_ops: pending_ops + 1}}
+
+  def handle_call(:enqueue_operation, _from, state),
+    do: {:reply, {:error, :too_many_requests_to_user}, state}
+
+  def handle_call(:dequeue_operation, _from, %State{pending_ops: pending_ops} = state)
+      when pending_ops > 0,
+      do: {:reply, {:ok, pending_ops - 1}, %State{state | pending_ops: pending_ops - 1}}
+
+  def handle_call(:dequeue_operation, _from, state),
+    do: {:reply, {:ok, 0}, %State{state | pending_ops: 0}}
+
+  defp via(name),
+    do: {:via, Registry, {ExBanking.UserRegistry, name}}
 end
--- lib/ex_banking/users/user_supervisor.ex
+++ lib/ex_banking/users/user_supervisor.ex
@@ -1,2 +1,23 @@
 defmodule ExBanking.Users.UserSupervisor do
+  @moduledoc false
+
+  # :temporary restart is used so that whenever a child process of UserSupervisor
+  # is consistently crashing and causing the UserSupervisor to exit, the exit will be trapped
+  # and leave the parent process (UserManager) to keep running.
+  use Supervisor, restart: :temporary
+
+  def start_link(args) do
+    Supervisor.start_link(__MODULE__, args)
+  end
+
+  def init(args) do
+    children = [
+      {ExBanking.Users.User, args}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one]
+    Supervisor.init(children, opts)
+  end
 end
