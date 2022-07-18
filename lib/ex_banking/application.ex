759fa6a0f3f9566b9ea1c3353828928bebdd7fa6
abiwinanda <abiwinanda@outlook.com>
2022-07-18 19:31:11 +0800

    implement Users module

--- lib/ex_banking/application.ex
+++ lib/ex_banking/application.ex
@@ -8,8 +8,8 @@ defmodule ExBanking.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: ExBanking.Worker.start_link(arg)
-      # {ExBanking.Worker, arg}
+      {Registry, keys: :unique, name: ExBanking.UserRegistry},
+      {DynamicSupervisor, strategy: :one_for_one, name: ExBanking.UserManager}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
--- lib/ex_banking/users.ex
+++ lib/ex_banking/users.ex
@@ -1,2 +1,39 @@
 defmodule ExBanking.Users do
+  @moduledoc """
+  TODO
+  """
+  alias ExBanking.{UserManager, UserRegistry}
+  alias ExBanking.Users.UserSupervisor
+
+  defguard is_valid_user(user) when is_binary(user) and user != ""
+
+  defp create_user_supervisor_process(user),
+    do: DynamicSupervisor.start_child(UserManager, {UserSupervisor, user})
+
+  @doc """
+  TODO
+  """
+  def create_user(user) when is_valid_user(user) do
+    case create_user_supervisor_process(user) do
+      {:ok, pid} ->
+        {:ok, pid}
+
+      {:error, {:shutdown, {:failed_to_start_child, _, {:already_started, _}}}} ->
+        {:error, :user_already_exists}
+    end
+  end
+
+  def create_user(_user), do: {:error, :wrong_arguments}
+
+  @doc """
+  TODO
+  """
+  def get_user_pid(user) when is_valid_user(user) do
+    case Registry.lookup(UserRegistry, user) do
+      [] -> {:ok, nil}
+      [{pid, _}] -> {:ok, pid}
+    end
+  end
+
+  def get_user_pid(_user), do: {:error, :wrong_arguments}
 end
