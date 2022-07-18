143b2d7a7a0da07ca3ce9f37ab0b8c5a436c2cb1
abiwinanda <abiwinanda@outlook.com>
2022-07-18 22:59:21 +0800

    add docs and specs for users context modules

--- lib/ex_banking/users.ex
+++ lib/ex_banking/users.ex
@@ -1,6 +1,6 @@
 defmodule ExBanking.Users do
   @moduledoc """
-  TODO
+  `ExBanking.Users` provides sets of functions to manage user related data in the bank.
   """
   alias ExBanking.{UserManager, UserRegistry}
   alias ExBanking.Users.UserSupervisor
@@ -11,8 +11,25 @@ defmodule ExBanking.Users do
     do: DynamicSupervisor.start_child(UserManager, {UserSupervisor, user})
 
   @doc """
-  TODO
+  Create a new user. The user must be a non empty string.
+
+  ## Examples
+
+      iex> create_user("user")
+      {:ok, #PID<0.106.0>}
+
+      iex> create_user("user")
+      {:error, :user_already_exists}
+
+      iex> create_user("")
+      {:error, :wrong_arguments}
+
+      iex> create_user(:user)
+      {:error, :wrong_arguments}
+
   """
+  @spec create_user(user :: String.t()) ::
+          {:ok, pid} | {:error, :wrong_arguments | :user_already_exists}
   def create_user(user) when is_valid_user(user) do
     case create_user_supervisor_process(user) do
       {:ok, pid} ->
@@ -26,8 +43,25 @@ defmodule ExBanking.Users do
   def create_user(_user), do: {:error, :wrong_arguments}
 
   @doc """
-  TODO
+  Get a process pid of a user. Return nil if the user does not exist.
+
+  ## Examples
+
+      iex> get_user_pid("user")
+      {:ok, #PID<0.106.0>}
+
+      iex> get_user_pid("non_existing_user")
+      {:ok, nil}
+
+      iex> get_user_pid("")
+      {:error, :wrong_arguments}
+
+      iex> get_user_pid(:user)
+      {:error, :wrong_arguments}
+
   """
+  @spec get_user_pid(user :: String.t()) ::
+          {:ok, pid | nil} | {:error, :wrong_arguments}
   def get_user_pid(user) when is_valid_user(user) do
     case Registry.lookup(UserRegistry, user) do
       [] -> {:ok, nil}
--- lib/ex_banking/users/user.ex
+++ lib/ex_banking/users/user.ex
@@ -1,5 +1,14 @@
 defmodule ExBanking.Users.User do
-  @moduledoc false
+  @moduledoc """
+  `ExBanking.Users.User` is a genserver process that represent a user entity in the bank. i.e one user is represented by a single genserver process.
+
+  `ExBanking.Users.User` genserver currently only has one state which is a `pending_ops` that tracks the number of
+  pending operation for a single user. This genserver controls or limits the concurrency of operations within a user.
+  You could think of this genserver as a synchronization point to provide back-pressure for a user. The limit of conccurency
+  within a given time for a single user is currently max to 10 pending operations. Any incoming operation above this limit will be rejected.
+
+  In order to extend the functionality of this genserver in the future, you could add more state later on.
+  """
   use GenServer
 
   defmodule State do
@@ -13,9 +22,42 @@ defmodule ExBanking.Users.User do
   def start_link(user),
     do: GenServer.start_link(__MODULE__, %State{}, name: via(user))
 
+  @doc """
+  Enqueue or increase the number of pending operations to a user. The maximum number of operation at a given time is 10,
+
+  ## Examples
+
+      iex> enqueue_operation("user")
+      {:ok, 1}
+
+      iex> enqueue_operation("user")
+      {:error, :too_many_requests_to_user}
+
+  """
+  @spec enqueue_operation(user :: String.t()) ::
+          {:ok, pending_ops :: number} | {:error, :too_many_requests_to_user}
   def enqueue_operation(user),
     do: GenServer.call(via(user), :enqueue_operation)
 
+  @doc """
+  Dequeue or decrease the number of pending operations to a user.
+
+  ## Examples
+
+      iex> dequeue_operation("user")
+      {:ok, 2}
+
+      iex> dequeue_operation("user")
+      {:ok, 1}
+
+      iex> dequeue_operation("user")
+      {:ok, 0}
+
+      iex> dequeue_operation("user")
+      {:ok, 0}
+
+  """
+  @spec dequeue_operation(user :: String.t()) :: {:ok, pending_ops :: number}
   def dequeue_operation(user),
     do: GenServer.call(via(user), :dequeue_operation)
 
