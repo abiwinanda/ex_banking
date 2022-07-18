757e3fea1be792ad5bcec766821f6e9e09fc0f77
abiwinanda <abiwinanda@outlook.com>
2022-07-18 21:44:05 +0800

    create helper functions for manual testing

--- .iex.exs
+++ .iex.exs
@@ -13,3 +13,35 @@ top_up_users_balance = fn ->
   end)
   |> Enum.map(&Task.await/1)
 end
+
+deposit_balance = fn ->
+  1..20
+  |> Enum.map(fn _ ->
+      Task.async(fn -> ExBanking.deposit("one", 1 ,"EUR") end)
+  end)
+  |> Enum.map(&Task.await/1)
+end
+
+deposit_balance_with_different_currencies = fn ->
+  1..20
+  |> Enum.map(fn i ->
+      Task.async(fn -> ExBanking.deposit("one", 1 ,"#{i}") end)
+  end)
+  |> Enum.map(&Task.await/1)
+end
+
+withdraw_balance = fn ->
+  1..15
+  |> Enum.map(fn _ ->
+      Task.async(fn -> ExBanking.withdraw("one", 1 ,"EUR") end)
+  end)
+  |> Enum.map(&Task.await/1)
+end
+
+get_balance = fn ->
+  1..15
+  |> Enum.map(fn _ ->
+      Task.async(fn -> ExBanking.get_balance("one", "EUR") end)
+  end)
+  |> Enum.map(&Task.await/1)
+end
